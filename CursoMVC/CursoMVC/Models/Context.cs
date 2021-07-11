aee7690fe3f91ae8d286db4c0a263ad0336872dd
Dcaiogsiqueira <85196475+Dcaiogsiqueira@users.noreply.github.com>
2021-07-11 12:31:47 -0300

    Implementação da Classe Produto e criação da tabela
    Foi implementada a classe produto, incluido o método de acesso no contexto, adicionado ao Migrations e criado no banco de dados a tabela.

--- CursoMVC/CursoMVC/Models/Context.cs
+++ CursoMVC/CursoMVC/Models/Context.cs
@@ -10,6 +10,7 @@ namespace CursoMVC.Models
     public class Context : DbContext
     {
         public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
         {
             optionBuilder.UseSqlServer(@"Data Source=DESKTOP-5KVLSJ6\SQLEXPRESS; Initial Catalog=CursoMVC; Persist Security Info=False;User ID=sa; Password=Indaia2006*");
