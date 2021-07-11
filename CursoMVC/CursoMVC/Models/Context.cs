e77e66ddc4a975bf9da5da951073ab45fef6c479
Dcaiogsiqueira <85196475+Dcaiogsiqueira@users.noreply.github.com>
2021-07-11 11:37:25 -0300

    Criação da string de conexão e do controller
    Criei a string de conexão na classe context, após ela foi dado Add-Migration InitialCreate e Update-Database para criação da tabela utilizando o model no banco de dados, após feito isto foi criada a classe controller para categoria utilizando scaffold, para que funcione foi atualizado o nuget  Install-Package Microsoft.CodeAnalysis.CSharp.Workspaces -Version 3.10.0

---
+++ CursoMVC/CursoMVC/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(@"Data Source=DESKTOP-5KVLSJ6\SQLEXPRESS; Initial Catalog=CursoMVC; Persist Security Info=False;User ID=sa; Password=Indaia2006*");
+        }
+    }
+}
